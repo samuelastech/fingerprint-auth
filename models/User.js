fe29a1803b71dff40a8a71bc666c2dd3bc6f3e93
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-10-06 19:01:21 -0300

    feat: user's model

--- models/User.js
+++ models/User.js
@@ -1,9 +1,14 @@
-class User {
-  constructor (name, email, password) {
-    this.name = name
-    this.email = email
-    this.password = password
-  }
-}
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  fingerprint: { data: Buffer, contentType: String },
+  accessLevel: { type: Number, enum: [1, 2, 2], required: true }
+})
+
+const User = mongoose.model('User', UserSchema)
 
 module.exports = User
