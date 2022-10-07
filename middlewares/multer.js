00ff35f4550028b4d150a20ab48d7117db643602
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-10-07 13:06:38 -0300

    fix: memory storage instead of disk storage

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,15 +1,5 @@
 const multer = require('multer')
-const path = require('path')
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, '../uploads')
-  },
-
-  filename: (req, file, callback) => {
-    const replacedName = Date.now() + path.extname(file.originalname)
-    callback(null, replacedName)
-  }
-})
+const storage = multer.memoryStorage()
 
 const upload = multer({ storage })
 
