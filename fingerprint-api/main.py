f5675aed35244dc810007e35735d0cc5436dee70
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-10-07 16:12:37 -0300

    feat: convert base64 to image

--- fingerprint-api/main.py
+++ fingerprint-api/main.py
@@ -1,1 +1,10 @@
-print('init')
+import os
+import cv2
+import numpy as np
+import base64
+
+def stringToImage(base64_string):
+    decoded = base64.b64decode(base64_string)
+    np_data = np.fromstring(decoded, np.uint8)
+    img = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
+    return img
