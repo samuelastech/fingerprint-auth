03ef2cbfd0bc326e3e96aadcf605170471de5d42
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-10-11 10:48:16 -0300

    feat: POST /authenticate need to verify if there's an image in the body

--- fingerprint-api/src/routes.py
+++ fingerprint-api/src/routes.py
@@ -1,5 +1,25 @@
-from src import app
+from email import message
+from src import app, User
+from flask import request, Response, jsonify
+import json
 
-@app.route('/')
-def hello_world():
-    return 'hello world'
+@app.route('/authenticate', methods=['POST'])
+def auth():
+    try:
+        if not 'image' in request.json:
+            raise Exception('you need to provide an image')
+        
+        image_base64 = request.json.image
+        
+        return Response(json.dumps({
+            'status': True,
+            'message': 'there is an image',
+            'image': image_base64
+        }), 200)
+        
+    except Exception as error:
+        if error == 'you need to provide an image':
+            return Response(json.dumps({
+                'status': False,
+                'message': str(error)
+            }), 404)
