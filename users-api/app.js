7364d87f8b72d0aa1cb990e17c1603704880770e
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-10-13 10:17:21 -0300

    fix: ejs not loading static files #4

--- users-api/app.js
+++ users-api/app.js
@@ -15,6 +15,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname, '/public')))
 
 const routes = require('./routes/__router__')
 routes(app)
