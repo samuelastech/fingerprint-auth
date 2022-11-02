c73fbc1d88ae76615519591fc8cb0e42989981eb
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-11-01 21:29:41 -0300

    feat: build the network

--- fingerprint-api/src/image_classifier.py
+++ fingerprint-api/src/image_classifier.py
@@ -50,6 +50,26 @@ train = data.take(train_size)
 val = data.skip(train_size).take(val_size)
 test = data.skip(train_size+val_size).take(test_size)
 
+# Building the Network
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+
+model = Sequential()
+
+model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+model.add(MaxPooling2D())
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+model.summary()
+
+
 '''
 fig, ax = plt.subplots(ncols=4, figsize=(20,20))
 for idx, img in enumerate(scaled_batch[0][:4]):
