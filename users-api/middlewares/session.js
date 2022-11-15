28671924e5545c61bee91e225a102df3e24030a8
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-11-14 22:01:19 -0300

    feat: redirect when JWT expired

--- users-api/middlewares/session.js
+++ users-api/middlewares/session.js
@@ -3,15 +3,21 @@ const User = require('../models/User')
 
 module.exports = {
   cookie: async (req, res, next) => {
-    const accessToken = req.cookies.Authorization
-    if (!accessToken) {
-      res.redirect('auth')
-    }
+    try {
+      const accessToken = req.cookies.Authorization
+      if (!accessToken) {
+        res.redirect('auth')
+      }
 
-    const secret = process.env.SECRET_PASS
-    const decoded = jwt.verify(accessToken, secret)
-    const user = await User.findById(decoded.id)
-    req.user = user
-    next()
+      const secret = process.env.SECRET_PASS
+      const decoded = jwt.verify(accessToken, secret)
+      const user = await User.findById(decoded.id)
+      req.user = user
+      next()
+    } catch (error) {
+      if (error.name === 'TokenExpiredError') {
+        res.redirect('auth')
+      }
+    }
   }
 }
