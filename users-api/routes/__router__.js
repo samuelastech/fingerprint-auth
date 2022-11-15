7026ba5621c61c56d0ffe4ea8eb4855398d79fa6
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-11-15 11:28:11 -0300

    feat: access level for certain informations

--- users-api/routes/__router__.js
+++ users-api/routes/__router__.js
@@ -1,11 +1,13 @@
 const index = require('./index')
 const users = require('./users')
 const login = require('./login')
+const info = require('./information')
 
 module.exports = (app) => {
   app.use(
     index,
     users,
-    login
+    login,
+    info
   )
 }
---
+++ users-api/routes/information.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const router = express.Router()
+
+/**
+ * Middlewares
+ */
+const session = require('../middlewares/session')
+
+router.get('/information/1', (req, res) => {
+  res.render('info/1')
+})
+
+router.get('/information/2', session.cookie, (req, res) => {
+  const { accessLevel } = req.user
+
+  // Unauthorized
+  if (accessLevel < 2) {
+    handleUnauthorized(res, [2, 3], accessLevel)
+  }
+
+  return res.render('info/2')
+})
+
+router.get('/information/3', session.cookie, (req, res) => {
+  const { accessLevel } = req.user
+
+  // Unauthorized
+  if (accessLevel < 3) {
+    return handleUnauthorized(res, [3], accessLevel)
+  }
+
+  return res.render('info/3')
+})
+
+/**
+ * Handle unauthorized access level
+ * @param {Array} requiredAccess access that can view the info -- [2, 3] [3]
+ * @param {Number} userAccess current user's access
+ * @returns {Object} response to the user
+ */
+function handleUnauthorized (res, requiredAccess, userAccess) {
+  return res.status(401).json({
+    status: false,
+    message: 'user not authorized to access this information',
+    requiredAccessLevel: requiredAccess,
+    yourAccessLevel: userAccess
+  })
+}
+
+module.exports = router
